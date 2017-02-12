affb9c3ca8853373b95d841d695d5091e5147076
Joseph Arhar <josepharhar@gmail.com>
2017-02-12 12:21:49 -0800

    Finished build script

--- server/build.sh
+++ server/build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-npm run scraper
+# generate files
+npm run scrape
 cp -r static build
+
+# push files
+cd build
+git init
+git add .
+git commit -m "gh-pages"
+git remote add origin git@github.com:josepharhar/meleeauthority
+git branch gh-pages
+git push -f origin gh-pages
+
+# clean files
+cd ..
+git clean -fdx build
+rm -rf build/.git

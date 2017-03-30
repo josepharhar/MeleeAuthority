0f5bcfcd77b4457c669ab2fd3093210eb82ccf3b
Joseph Arhar <josepharhar@gmail.com>
2017-03-30 11:50:51 -0700

    alphabetic three column characters list

--- server/views/CharactersListLayout.jsx
+++ server/views/CharactersListLayout.jsx
@@ -6,12 +6,23 @@ var BodyScripts = require('./BodyScripts');
 var CharactersListLayout = React.createClass({
   render: function() {
     var characters = this.props.characters;
-    var buttons = Object.keys(characters).sort().map(function(charId) {
+    var numCharacters = Object.keys(characters).length;
+
+    var buttons = Object.keys(characters).map(function(charId) {
       return {
         name: characters[charId],
         link: '/characters/' + charId
       };
+    }).sort(function(one, two) {
+      return one.name.localeCompare(two.name);
     });
+
+    var one_third = Math.ceil(numCharacters / 3);
+    var two_thirds = Math.ceil(2 * numCharacters / 3);
+
+    var buttons_one = buttons.slice(0, one_third);
+    var buttons_two = buttons.slice(one_third, two_thirds);
+    var buttons_three = buttons.slice(two_thirds);
 
     return (
       <html>
@@ -22,7 +33,17 @@ var CharactersListLayout = React.createClass({
         <body>
           <div className="container">
             <h1>Characters List</h1>
-            <ButtonList buttons={buttons}/>
+            <div className="flex-container">
+              <div className="flex-item">
+                <ButtonList buttons={buttons_one}/>
+              </div>
+              <div className="flex-item">
+                <ButtonList buttons={buttons_two}/>
+              </div>
+              <div className="flex-item">
+                <ButtonList buttons={buttons_three}/>
+              </div>
+            </div>
           </div>
           <BodyScripts />
         </body>

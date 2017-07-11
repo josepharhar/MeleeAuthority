51fa7be768491c246b5760ab5af17be75b3435a1
Joseph Arhar <josepharhar@gmail.com>
2017-07-11 03:20:02 +0000

    started on table component

---
+++ server/client-src/table.js
@@ -0,0 +1,22 @@
+class Table extends React.Component {
+  // props.colname.entryname = alternateentryname
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  sort() {
+
+  }
+
+  filter(regex) {
+
+  }
+
+  render() {
+
+  }
+}
+
+module.exports = Table;

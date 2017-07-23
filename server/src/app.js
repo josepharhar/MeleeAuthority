c38c71956a30d09c5e4e60f8d213ccb41ac08699
Joseph Arhar <josepharhar@gmail.com>
2017-07-23 22:01:13 +0000

    removed old app.js

--- server/src/app.js
+++
@@ -1,7 +0,0 @@
-var MyComponent = React.createClass({
-  render: function() {
-    return <blockquote>this is a quote lol</blockquote>;
-  }
-});
-
-ReactDOM.render(<MyComponent />, document.getElementById('app'));

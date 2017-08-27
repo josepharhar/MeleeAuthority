2f076b5526f4d10eb757f7c692f6d786bd3da715
Joseph Arhar <josepharhar@gmail.com>
2017-08-27 13:16:09 -0700

    removed navbar comments

--- server/client-src/navbar.js
+++ server/client-src/navbar.js
@@ -45,27 +45,6 @@ class Navbar extends React.Component {
       </nav>
     );
   }
-					/*<div className="navbar-header">
-						<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-							<span className="sr-only">Toggle navigation</span>
-							<span className="icon-bar"></span>
-							<span className="icon-bar"></span>
-							<span className="icon-bar"></span>
-						</button>
-						<a className="navbar-brand" href="/">Melee Authority</a>
-					</div>*/
-							/*<li className="dropdown">
-								<a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span className="caret"></span></a>
-								<ul className="dropdown-menu" role="menu">
-									<li><a href="#">Action</a></li>
-									<li><a href="#">Another action</a></li>
-									<li><a href="#">Something else here</a></li>
-									<li className="divider"></li>
-									<li><a href="#">Separated link</a></li>
-									<li className="divider"></li>
-									<li><a href="#">One more separated link</a></li>
-								</ul>
-							</li>*/
 }
 
 ReactDOM.render(<Navbar />, document.getElementById('navbar'));

ed521575d314d86ea730aecc915d7568af0cd8da
Joseph Arhar <josepharhar@gmail.com>
2017-08-30 20:44:37 -0700

    moved jquery to static

--- server/views/BodyScripts.jsx
+++ server/views/BodyScripts.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 class BodyScripts extends React.Component {
   render() {
     return (<div>
-      <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script src="/static/jquery-3.2.1.min.js"></script>
       <script src="/static/bootstrap.min.js"></script>
       <script src="/static/react.js"></script>
       <script src="/static/react-dom.js"></script>
